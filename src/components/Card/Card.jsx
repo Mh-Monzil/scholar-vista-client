5f8b6f486789bb877d97aa72d8a3e2a7395b0d5d
Mh-Monzil <monzil246@gmail.com>
2024-06-05 17:35:27 +0600

    top scholarship section added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,11 @@
 import YellowButton from "../Shared/YellowButton";
 import { IoLocationSharp } from "react-icons/io5";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
 
 const Card = ({ scholar }) => {
-  const {_id,
+  const {
+    _id,
     universityName,
     universityImage,
     scholarshipCategory,
@@ -13,11 +15,24 @@ const Card = ({ scholar }) => {
     applicationFees,
     rating,
   } = scholar;
+
+  const ratingStar = {
+    size: 25,
+    value: rating,
+    edit: false,
+    activeColor: "#F2A227",
+  };
+
   return (
     <div className="card mx-auto rounded-md w-full bg-base-100 shadow-md ">
       <figure className="relative overflow-hidden rounded-ee-[70px]">
-        <img className="w-full h-60 hover:scale-105 transition-all duration-300" src={`${universityImage}.jpg`} />
-        <span className="absolute bg-navy text-white top-4 right-4 px-3 py-1 rounded-md text-sm">{subjectCategory}</span>
+        <img
+          className="w-full h-60 hover:scale-105 transition-all duration-300"
+          src={`${universityImage}.jpg`}
+        />
+        <span className="absolute bg-navy text-white top-4 right-4 px-3 py-1 rounded-md text-sm">
+          {subjectCategory}
+        </span>
       </figure>
       <div className="card-body">
         <h2 className="card-title text-navy">{universityName}</h2>
@@ -25,12 +40,22 @@ const Card = ({ scholar }) => {
           <IoLocationSharp />
           <span>{universityLocation}</span>
         </p>
-        <p><span className="font-medium">Deadline:</span> {applicationDeadline} </p>
-        <p><span className="font-medium">Scholarship Category:</span> {scholarshipCategory} </p>
-        <p className="text-2xl font-semibold text-navy">{applicationFees}</p>
+        <p>
+          <span className="font-medium">Deadline:</span> {applicationDeadline}{" "}
+        </p>
+        <p>
+          <span className="font-medium">Scholarship Category:</span>{" "}
+          {scholarshipCategory}{" "}
+        </p>
+        <div className="flex items-center justify-between py-2">
+          <p className="text-3xl font-semibold text-navy">{applicationFees}</p>
+          <span>
+            <ReactStars {...ratingStar} />
+          </span>
+        </div>
         {/* <div className="justify-end"> */}
         <Link to={`/scholarship-details/${_id}`}>
-        <YellowButton label={"View Details"} />
+          <YellowButton label={"View Details"} />
         </Link>
         {/* </div> */}
       </div>
@@ -39,5 +64,3 @@ const Card = ({ scholar }) => {
 };
 
 export default Card;
-
-
--- src/pages/Home/TopScholarship.jsx
+++ src/pages/Home/TopScholarship.jsx
@@ -1,12 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
-
+import UseAxiosPublic from "../../hooks/useAxiosPublic";
+import Card from "../../components/Card/Card";
 
 const TopScholarship = () => {
-    return (
-        <div className="max-w-7xl mx-auto text-center">
-            <SectionTitle  title={"Top Scholarship"} description={"Discover the pinnacle of educational opportunities! Explore our curated selection of top scholarships, opening doors to academic excellence and success."} />
-        </div>
-    );
+  const axiosPublic = UseAxiosPublic();
+
+  const { data: scholarships = [], isLoading } = useQuery({
+    queryKey: ["scholarships"],
+    queryFn: async () => {
+      const { data } = await axiosPublic.get("/top-scholarships");
+      console.log(data);
+      return data.slice(0, 6);
+    },
+  });
+
+  return (
+    <div className="max-w-7xl mx-auto mt-12 lg:mt-24">
+      <SectionTitle
+        title={"Top Scholarship"}
+        description={
+          "Discover the pinnacle of educational opportunities! Explore our curated selection of top scholarships, opening doors to academic excellence and success."
+        }
+      />
+      <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 xl:gap-10">
+        {scholarships?.map((scholar) => (
+          <Card key={scholar._id} scholar={scholar} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TopScholarship;
